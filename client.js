9421c109038ddf19a95ca1406e3ce541aabacf87
cytusalive <cytusomega@gmail.com>
2021-07-01 04:44:21 +0000

    set up input

--- client.js
+++ client.js
@@ -12,8 +12,14 @@ const connect = function () {
   // interpret incoming data as text
   conn.setEncoding("utf8");
   conn.on('data', data => console.log('Server:', data));
+  
   return conn;
   
 };
 
 module.exports = connect;
+
+// "Move: up" - move up one square (unless facing down)
+// "Move: down" - move down one square (unless facing up)
+// "Move: left" - move left one square (unless facing right)
+// "Move: right" - move left one square (unless facing left)
